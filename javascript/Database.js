81324cf4961575356233e4d37f10b54077f7a024
Sarah Rosenthal <sarahrosenthal@Sarahs-MacBook-Air.local>
2016-03-21 21:55:28 -0400

    adding DB file

--- javascript/Database.js
+++ javascript/Database.js
@@ -1,6 +1,18 @@
 var Firebase = require('firebase');
 
-module.exports = {
-    test : function() {
+var Database = (function() {
+
+    var _db;
+
+    function initDb() {
+        _db = new Firebase('https://brilliant-inferno-2015.firebaseio.com');
+        console.log( _db );
     }
-}
+
+    return {
+        init : initDb
+    }
+
+})();
+
+module.exports = Database;
